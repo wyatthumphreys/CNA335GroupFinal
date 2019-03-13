58da04c994a2c6274efde802fb78df10fd2c1bca
Wyatt <wlhumphreys@student.rtc.edu>
2019-03-12 20:24:09 -0700

    added support for file parse

--- Google_Search_Randomizer.py
+++ Google_Search_Randomizer.py
@@ -1,8 +1,26 @@
 from flask import Flask, redirect, url_for, request, render_template
+import random
 
 app = Flask(__name__, static_url_path='')
 
-print("test")
+linelist=[]
+randolist=[]
+counter=4
+
+#http://www.google.com/search?q=variable&btnl
+
+@app.route('/usenumber', methods=['POST'])
+def usenumber(counter, linelist, randolist):
+    with open('words.txt', 'rt') as wordfile:
+        for line in wordfile:
+            linelist.append(line)
+        while counter != 0:
+            testvar = random.choice(linelist)
+            randolist.append(testvar)
+            counter = counter - 1
+            print(randolist)
+            
+
 @app.route('/SelectedNumber', methods=['GET'])
 def SelectedNumber():
        user = request.args.get('szip')
